086ca0c2afcdc407c356f64844fb7a44acccdcd3
Damien LAGAE <damienlagae@gmail.com>
2025-10-19 17:22:03 +0200

    ✨ Add modal

--- src/CyclingAppsComponentBundle.php
+++ src/CyclingAppsComponentBundle.php
@@ -10,7 +10,10 @@
 namespace CyclingApps\ComponentBundle;
 
 use CyclingApps\ComponentBundle\DependencyInjection\CyclingAppsComponentExtension;
+use Symfony\Component\AssetMapper\AssetMapperInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class CyclingAppsComponentBundle extends AbstractBundle
@@ -18,6 +21,31 @@ class CyclingAppsComponentBundle extends AbstractBundle
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+
+    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        if (!interface_exists(AssetMapperInterface::class)) {
+            return;
+        }
+
+        $metadata = $builder->getParameter('kernel.bundles_metadata');
+
+        if (!\is_array($metadata) || !isset($metadata['FrameworkBundle'])) {
+            return;
+        }
+
+        if (!is_file($metadata['FrameworkBundle']['path'].'/Resources/config/asset_mapper.php')) {
+            return;
+        }
+
+        $builder->prependExtensionConfig('framework', [
+            'asset_mapper' => [
+                'paths' => [
+                    __DIR__.'/../assets/dist',
+                ],
+            ],
+        ]);
     }
 
     public function getContainerExtension(): ?ExtensionInterface
