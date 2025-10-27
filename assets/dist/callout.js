e3759359438dc5a8e1382d249c1f9f2531cefb75
Damien LAGAE <damienlagae@gmail.com>
2025-10-27 18:53:24 +0100

    🐛 JS callout & widget

---
+++ assets/dist/callout.js
@@ -0,0 +1,6 @@
+import "./callout.css";
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+
+}
---
+++ assets/dist/widget.js
@@ -0,0 +1,6 @@
+import "./widget.css";
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+
+}
